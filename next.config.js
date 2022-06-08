4b1f9a0e17118468b69e2cc645bf6feea634e682
hakamfaza <hakamfaza@gmail.com>
2022-06-08 12:31:08 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: []
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  }
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: []
+    },
+    colors: {
+      primary: '#FFFFFF',
+      secondary: '#F01F0E',
+      tertiray: '#2AA952'
+    }
+  },
+  plugins: [],
+}
