26da9ef7a2bda696bac452ff73b15f56ee129076
hakamfaza <hakamfaza@gmail.com>
2022-06-08 16:38:26 +0700

    add global color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,10 @@ module.exports = {
     colors: {
       primary: '#FFFFFF',
       secondary: '#F01F0E',
-      tertiray: '#2AA952'
+      tertiray: '#2AA952',
+      dark: '#000000',
+      gray: '#9B9B9B'
     }
   },
-  plugins: [],
-}
+  plugins: []
+};
