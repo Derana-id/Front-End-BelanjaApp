a88a8c56aea7a9c9fc2b37073c367c6923eb5c40
hanifudin0597 <hanifudin0597@gmail.com>
2022-06-09 06:34:30 +0700

    responsive auth

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Input from '../../components/Input'
@@ -7,18 +7,20 @@ import Button from '../../components/Button'
 
 export default function index() {
     const [formShow, setFormShow] = useState(0)
+    const [activeButton, setActiveButton] = useState()
 
     const setCurrentShow = index => {
         setFormShow(index);
+        // setActiveButton()
     };
     return (
         <div className='flex min-h-screen'>
-            <div className='w-2/6 border-slate-200 rounded-xl mx-auto p-5 flex flex-col items-center relative' >
+            <div className='sm:w-full md:w-2/6 border-slate-200 rounded-xl mx-auto p-5 flex flex-col items-center relative' >
                 <Image src='/logoAuth.svg' width={150} height={100} layout='fixed' />
                 <label className='font-bold' >Please login with your account</label>
                 <div className='flex w-[230px] flex-row my-8'>
-                    <button onClick={() => setCurrentShow(0)} className='border-[1px] rounded-l text-gray w-full h-12 focus:bg-special-warning focus:text-white'>customer</button>
-                    <button onClick={() => setCurrentShow(1)} className='border-[1px] rounded-r text-gray w-full h-12 focus:bg-special-warning focus:text-white '>Seller</button>
+                    <button onClick={() => setCurrentShow(0)} id='button-1' className='border-[1px] rounded-l text-gray w-full active:bg-primary h-12 focus:bg-special-warning focus:text-white'>customer</button>
+                    <button onClick={() => setCurrentShow(1)} id='' className='border-[1px] rounded-r text-gray w-full h-12 focus:bg-special-warning focus:text-white '>Seller</button>
                 </div>
                 {
                     formShow === 0 ? (
@@ -27,7 +29,7 @@ export default function index() {
                             <Input placeholder='Email' />
                             <Input placeholder='Password' />
                             <Button name='Register' />
-                            <label className='ml-12 mr-2'>Already have a Tokopedia account?</label>
+                            <label className='ml-2 sm:ml-2 md:ml-12 lg:ml-12 mr-2'>Already have a Tokopedia account?</label>
                             <Link href='/auth/login' className='text-special-warning'>Login</Link>
                         </form>
                     ) : (
@@ -38,7 +40,7 @@ export default function index() {
                             <Input placeholder='Store name' />
                             <Input placeholder='Password' />
                             <Button name='Register' />
-                            <label className='ml-12 mr-2'>Already have a Tokopedia account?</label>
+                            <label className='ml-2 sm:ml-2 md:ml-12 lg:ml-12 mr-2'>Already have a Tokopedia account?</label>
                             <Link href='/auth/login' className='text-special-warning'>Login</Link>
                         </form>
                     )
