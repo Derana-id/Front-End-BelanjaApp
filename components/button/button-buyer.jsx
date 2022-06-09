a789558869ce146ea0511f4382109a400374334a
hakamfaza <hakamfaza@gmail.com>
2022-06-09 11:35:30 +0700

    update product section

--- components/button/button-buyer.jsx
+++ components/button/button-buyer.jsx
@@ -4,7 +4,7 @@ export default function ButtonBuyyer(params) {
   return (
     <div>
       <button
-        className="bg-primary w-full rounded-full p-3 text-white font-bold font-lg hover:bg-white hover:border-2 hover:border-solid hover:border-primary hover:text-primary"
+        className="bg-primary w-full rounded-full p-[10px] text-white font-bold font-lg hover:bg-white hover:border-2 hover:border-solid hover:border-primary hover:text-primary"
         onClick={params.onClick}
       >
         {params.action}
--- components/navbar/navbar-main.jsx
+++ components/navbar/navbar-main.jsx
@@ -10,19 +10,19 @@ import vector from '../../assets/icons/vector.png';
 export default function MainNavbar() {
   return (
     <div className="w-full h-20 px-28 flex flex-row items-center fixed z-10 shadow-lg bg-white">
-      <div class="flex w-full items-center">
-        <div class="w-1/5 h-12 flex items-center">
+      <div className="flex w-full items-center">
+        <div className="w-1/5 h-12 flex items-center">
           <div className="relative flex items-center">
             <Image src={logo} className="w-14 h-4" width={110} height={40} />
           </div>
         </div>
-        <div class="w-3/5 h-12 flex items-center">
+        <div className="w-3/5 h-12 flex items-center">
           <SearchNavbar />
           <div className="border-solid border-2 border-gray rounded-xl m-3 flex items-center p-2 justify-center">
             <Image src={vector} width={20} height={20} />
           </div>
         </div>
-        <div class="w-1/5 h-12 flex items-center">
+        <div className="w-1/5 h-12 flex items-center">
           <div className="flex justify-end w-full h-full items-center">
             <AiOutlineShoppingCart className="text-2xl m-4 text-gray cursor-pointer" />
             <MdOutlineNotificationsNone className="text-2xl m-4 text-gray cursor-pointer" />
