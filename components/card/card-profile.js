9403a20300d81d163540d56767e3aa8eec7fc2fd
hanifudin0597 <hanifudin0597@gmail.com>
2022-06-09 15:08:57 +0700

    proces page profile

---
+++ components/card/card-profile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function cardProfile() {
+  return (
+    <div className='bg-white rounded w-3/4 h-auto my-auto'>
+      geelo
+    </div>
+  )
+}
--- pages/profile/seller.js
+++ pages/profile/seller.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 export default function seller() {
     return (
-        <div>
-
-        </div>
+        <div>seller</div>
     )
 }
