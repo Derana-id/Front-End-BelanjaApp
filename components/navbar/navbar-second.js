af4ec452f8bc73d892f20a00ed103ed6fdba34ac
hakamfaza <hakamfaza@gmail.com>
2022-06-11 00:20:31 +0700

    add notification

--- components/navbar/navbar-second.js
+++ components/navbar/navbar-second.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import notification from '../../assets/img/notification.png';
 import logo from '../../assets/img/logo.png';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { MdOutlineNotificationsNone } from 'react-icons/md';
@@ -8,6 +9,15 @@ import { HiOutlineMail } from 'react-icons/hi';
 import user from '../../assets/img/user.jpg';
 
 export default function SecondNavbar() {
+  const [isActive, setActive] = useState(false);
+
+  const getActive = e => {
+    if (isActive) {
+      setActive(false);
+    } else {
+      setActive(true);
+    }
+  };
   return (
     <div className="w-full h-20 px-28 flex flex-row items-center fixed z-10 shadow-lg bg-white">
       <div className="flex w-full items-center">
@@ -30,7 +40,10 @@ export default function SecondNavbar() {
             </Link>
             <Link href="">
               <div>
-                <MdOutlineNotificationsNone className="text-2xl m-4 text-gray cursor-pointer" />
+                <MdOutlineNotificationsNone
+                  className="text-2xl m-4 text-gray cursor-pointer"
+                  onClick={() => getActive()}
+                />
               </div>
             </Link>
             <Link href="/chat">
@@ -44,6 +57,11 @@ export default function SecondNavbar() {
           </div>
         </div>
       </div>
+      {isActive ? (
+        <div className="absolute h-80 w-56 bg-white z-40 top-14 p-5 rounded-b-xl rounded-tl-xl shadow-lg right-60 flex justify-center items-center">
+          <Image src={notification} width={170} height={150} />
+        </div>
+      ) : null}
     </div>
   );
 }
