63cc81a4b30dcf55d3321d5522c95fd7206f2362
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-10 20:42:10 +0700

    Auth Integration

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -4,15 +4,27 @@ import Image from 'next/image';
 import Input from '../../components/Input';
 import Link from 'next/link';
 import Button from '../../components/Button';
+import { AuthSwitch } from '../../components';
 
 export default function index() {
+  const [level, setLevel] = useState(1);
   const [formShow, setFormShow] = useState(0);
+
+  const initialState = {
+    email: '',
+    password: ''
+  };
+  const [form, setForm] = useState(initialState);
+
+  const changeSwitchHandler = e => {
+    setLevel(e.target.value);
+  };
 
   const setCurrentShow = index => {
     setFormShow(index);
   };
   return (
-    <div className="flex align-items-center min-h-screen">
+    <div className="flex min-h-screen">
       <Head>
         <title>Belanja | Login</title>
         <meta name="" content="" />
@@ -21,20 +33,7 @@ export default function index() {
       <div className="sm:w-full md:w-2/6 border-slate-200 rounded-xl mx-auto p-5 flex flex-col items-center relative">
         <Image src="/logoAuth.svg" width={150} height={100} layout="fixed" />
         <label className="font-bold">Please login with your account</label>
-        <div className="flex w-[230px] flex-row my-8">
-          <button
-            onClick={() => setCurrentShow(0)}
-            className="border-solid-gray rounded text-gray w-full h-12 focus:bg-special-warning focus:text-white"
-          >
-            customer
-          </button>
-          <button
-            onClick={() => setCurrentShow(1)}
-            className="border-solid-gray rounded text-gray w-full h-12 focus:bg-special-warning focus:text-white "
-          >
-            Seller
-          </button>
-        </div>
+        <AuthSwitch />
         {formShow === 0 ? (
           <form className="w-full">
             <Input placeholder="Email" />
