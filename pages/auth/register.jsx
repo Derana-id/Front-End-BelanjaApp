a92e05e8a51beda1fc9ab9c8337eac36a5072bf5
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-11 10:27:28 +0700

    register integration

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { toastify } from '../../utils/toastify';
+import { sweetAlert } from '../../utils/sweetalert';
+import { registerBuyer, registerSeller } from '../../redux/actions/auth';
 import Head from 'next/head';
 import Image from 'next/image';
 import Input from '../../components/Input';
@@ -6,13 +10,113 @@ import Link from 'next/link';
 import Button from '../../components/Button';
 
 export default function index() {
+  const router = useRouter();
   const [formShow, setFormShow] = useState(0);
-  const [activeButton, setActiveButton] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    store_name: '',
+    store_phone: ''
+  };
+  const [form, setForm] = useState(initialState);
 
   const setCurrentShow = index => {
+    setForm({
+      name: '',
+      email: '',
+      password: '',
+      store_name: '',
+      store_phone: ''
+    });
     setFormShow(index);
-    // setActiveButton()
   };
+
+  const { name, email, password, store_name, store_phone } = form;
+
+  const handleChange = e => {
+    setForm({
+      ...form,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmitCustomer = e => {
+    e.preventDefault();
+    if (!name || !email || !password) {
+      sweetAlert('All field must be filled!', 'error');
+    } else {
+      setIsLoading(true);
+      registerBuyer({
+        name,
+        email,
+        password
+      })
+        .then(res => {
+          router.push('/auth/login');
+          sweetAlert(res.message);
+        })
+        .catch(err => {
+          if (err.response.data.code === 422) {
+            const { error } = err.response.data;
+            error.map(e => toastr(e, 'error'));
+          } else {
+            sweetAlert(err.response.data.message, 'error');
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+          setForm({
+            name: '',
+            email: '',
+            password: '',
+            store_name: '',
+            store_phone: ''
+          });
+        });
+    }
+  };
+
+  const handleSubmitSeller = e => {
+    e.preventDefault();
+    if (!name || !email || !password || !store_name || !store_phone) {
+      sweetAlert('All field must be filled!', 'error');
+    } else {
+      setIsLoading(true);
+      registerSeller({
+        name,
+        email,
+        store_name,
+        store_phone,
+        password
+      })
+        .then(res => {
+          router.push('/auth/login');
+          sweetAlert(res.message);
+        })
+        .catch(err => {
+          if (err.response.data.code === 422) {
+            const { error } = err.response.data;
+            error.map(e => toastr(e, 'error'));
+          } else {
+            sweetAlert(err.response.data.message, 'error');
+          }
+        })
+        .finally(() => {
+          setIsLoading(false);
+          setForm({
+            name: '',
+            email: '',
+            password: '',
+            store_name: '',
+            store_phone: ''
+          });
+        });
+    }
+  };
+
   return (
     <div className="flex min-h-screen">
       <Head>
@@ -55,24 +159,24 @@ export default function index() {
           </div>
         )}
         {formShow === 0 ? (
-          <form className="w-full">
-            <Input placeholder="Name" />
-            <Input placeholder="Email" />
-            <Input placeholder="Password" />
-            <Button name="Register" />
+          <form className="w-full" onSubmit={handleSubmitCustomer}>
+            <Input placeholder="Name" id="name" type="text" value={name} onChange={handleChange} />
+            <Input placeholder="Email" id="email" type="email" value={email} onChange={handleChange} />
+            <Input placeholder="Password" id="password" type="password" value={password} onChange={handleChange} />
+            {isLoading ? <Button disabled="disabled" name="Loading" /> : <Button name="Register" type="submit" />}
             <label className="ml-2 sm:ml-2 md:ml-12 lg:ml-12 mr-2">Already have a Tokopedia account?</label>
             <Link href="/auth/login" className="text-special-warning">
               Login
             </Link>
           </form>
         ) : (
-          <form className="w-full">
-            <Input placeholder="Name" />
-            <Input placeholder="Email" />
-            <Input placeholder="Phone" />
-            <Input placeholder="Store name" />
-            <Input placeholder="Password" />
-            <Button name="Register" />
+          <form className="w-full" onSubmit={handleSubmitSeller}>
+            <Input placeholder="Name" id="name" type="text" value={name} onChange={handleChange} />
+            <Input placeholder="Email" id="email" type="email" value={email} onChange={handleChange} />
+            <Input placeholder="Phone" id="store_phone" type="text" value={store_phone} onChange={handleChange} />
+            <Input placeholder="Store name" id="store_name" type="text" value={store_name} onChange={handleChange} />
+            <Input placeholder="Password" id="password" type="password" value={password} onChange={handleChange} />
+            {isLoading ? <Button disabled="disabled" name="Loading" /> : <Button name="Register" type="submit" />}
             <label className="ml-2 sm:ml-2 md:ml-12 lg:ml-12 mr-2">Already have a Tokopedia account?</label>
             <Link href="/auth/login" className="text-special-warning">
               Login
