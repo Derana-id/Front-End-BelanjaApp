3589eb5e7b99c1261a8e9afc531995ef11597c41
hakamfaza <hakamfaza@gmail.com>
2022-06-12 11:30:11 +0700

    add filter

--- components/molecules/color.js
+++ components/molecules/color.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 export default function Color(params) {
   return (
-    <button className="focus:border-solid focus:border-2 focus:border-primary p-1 rounded-full m-1">
+    <button
+      className="focus:border-solid focus:border-2 focus:border-primary p-1 rounded-full m-1"
+      onClick={params.onClick}
+    >
       <div
         className={`w-9 h-9 ${params.color} rounded-full focus:border-2 focus:border-solid focus:border-dark cursor-pointer`}
       />
